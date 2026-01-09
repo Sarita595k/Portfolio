b83c8a8ad1eca23213ed175245db17e82c99981d
sarita595k <sarita595k@gmail.com>
2026-01-09 10:35:21 +0530

    sub container, main container created

---
+++ Frontend/src/Components/Header/MainContainer.jsx
@@ -0,0 +1,24 @@
+export const MainContainer = () => {
+    return (
+        <>
+            <div id="mainContainer">
+                <div class="container">
+
+                    <div id="greetings-container"></div>
+                    <section id="top-hello-section">
+                        <p>Welcome to my portfolio<span><i class="fa-solid fa-heart"></i></span></p>
+                    </section>
+                    {/* <!-- <div class="top-name">
+                        <span>made with love</span>
+                        <span>by</span>
+                        <p>sarita</p>
+                    </div> --> */}
+                </div>
+                <div class="plane"></div>
+                <div class="imageContainer">
+                    <img src="./images/wave-image.png" alt="girls-header-image" />
+                </div>
+            </div>
+        </>
+    )
+}
