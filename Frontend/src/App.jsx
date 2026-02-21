fd111601bd2f2e6af3291e6f7b81dc9f7f8b0b03
sarita595k <sarita595k@gmail.com>
2026-02-21 19:38:46 +0530

    small changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MainContainer from './Components/Header/MainContainer'
 import SolarSystem from './Components/Header/solarSystem'
